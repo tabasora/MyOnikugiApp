0c2a76ea27346724e74faab89fecc6a3fbe06ed4
Macbook <macbook@macbooknomacbook-puro.local>
2020-11-28 15:01:33 +0900

    Initial Commit

---
+++ MyOnikugiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyOnikugiApp
+//
+//  Created by Macbook on 2020/11/28.
+//  Copyright © 2020年 Tabata Sorane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
